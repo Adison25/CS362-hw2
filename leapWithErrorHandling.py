5b86ef6b4d4bee7aa3cc2384552afa225f2065bb
Adison25 <emerickadison@gmail.com>
2021-04-21 07:47:45 -0700

    sike I forgot to transfer over my code.
    NOw this is it

--- leapWithErrorHandling.py
+++ leapWithErrorHandling.py
@@ -1,10 +1,18 @@
 print("Program checking to see if a year is a leap year or not")
-year = input("Please enter a year: ")  
-if year % 4 != 0:
-    print(str(year) + " is a common year")
-elif year % 100 != 0:
-    print(str(year) + " is a leap year")
-elif year % 400 != 0:
-    print(str(year) + " is a common year")
-else:   
-    print(str(year) + " is a leap year")
+run = 1
+while run == 1:
+    year = input("Please enter a year: ") 
+    #check input
+    if year.isnumeric():
+    #if info is valid 
+        if int(year) % 4 != 0:
+            print(str(year) + " is a common year")
+        elif int(year) % 100 != 0:
+            print(str(year) + " is a leap year")
+        elif int(year) % 400 != 0:
+            print(str(year) + " is a common year")
+        else:   
+            print(str(year) + " is a leap year")
+        break
+    else:
+        print("Please enter an integer!\n")
